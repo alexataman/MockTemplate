c1a068c43c5e26a3adbb039ead32e4edfbb4b9c6
Alexander Atamanskyi <oleksandr.atamanskyi@ekreative.com>
2017-12-28 12:58:38 +0200

    First commit

---
+++ ___FILEBASENAME___.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+@testable import ___PROJECTNAME___
+
+class ___FILEBASENAMEASIDENTIFIER___: Mock {
+
+	var wasCalled: Bool = false
+
+	// Custom implementation here
+
+}
